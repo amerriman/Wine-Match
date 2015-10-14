6f6023352ea60695094bbffa1ca3e15a133d4000
amerriman <amm434@gmail.com>
2015-10-13 21:08:53 -0600

    removed social auth

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 // *** main dependencies *** //
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
--- server/routes/socialAPI.js
+++ server/routes/socialAPI.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-var passportGithub = require('../auth/github');
-
-
-router.get('/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }));
-
-router.get('/github/callback',
-  passportGithub.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication
-    res.json(req.user);
-  });
+// var passportGithub = require('../auth/github');
 
 
+// router.get('/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }));
 
-module.exports = router;
+// router.get('/github/callback',
+//   passportGithub.authenticate('github', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     // Successful authentication
+//     res.json(req.user);
+//   });
+
+
+
+// module.exports = router;
 
