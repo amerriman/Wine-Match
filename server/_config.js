9825ff7ea6df491d1a4f1d682e575827df36634f
amerriman <amm434@gmail.com>
2015-10-13 18:10:59 -0600

    heroku

--- server/_config.js
+++ server/_config.js
@@ -2,7 +2,8 @@ var config = {};
 
 config.mongoURI = {
   development: 'mongodb://localhost/wine',
-  test: 'mongodb://localhost/wine-test'
+  test: 'mongodb://localhost/wine-test',
+  production: process.env.MONGOLAB_URI
 };
 
 module.exports = config;
