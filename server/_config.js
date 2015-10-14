04d87874cc3a3e2297e1ab911afee681cc86fa72
amerriman <amm434@gmail.com>
2015-10-13 18:46:39 -0600

    testing

--- server/_config.js
+++ server/_config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   development: 'mongodb://localhost/wine',
   test: 'mongodb://localhost/wine-test',
-  production: process.env.MONGOLAB_URI
+  production: 'process.env.MONGOLAB_URI'
 };
 
 module.exports = config;
